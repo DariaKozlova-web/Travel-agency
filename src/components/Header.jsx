fdb67cffa72267041aac34d2a4f17dbde4f8ec9c
Daria Kozlova <kozloffa.daria@gmail.com>
2025-11-27 16:07:02 +0100

    new src for logo in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router";
+import logo from "../assets/images/travel-agency-logo.png";
+
 export const Header = () => {
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="flex-1">
         <a className="btn btn-ghost text-xl text-amber-600">
-          <img className="h-10" src="../assets/images/travel-agency-logo.png" alt="Travel Agency" />Travel Agency</a>
+          <img className="h-10" src={logo} alt="Travel Agency" />Travel Agency</a>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
